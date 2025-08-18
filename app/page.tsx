6b43e94955137d47d469b480816b84cfb7a7dfd5
emilyl2008 <emilylimarib1638@gmail.com>
2025-08-18 14:56:48 -0300

    removing tsconfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
 import Image from 'next/image';
-
+//js aqui
 export default function Home() {
+  //js aqui
+
+  class Pessoa {
+    constructor(nome, idade) {
+      this.nome = nome;
+      this.idade = idade;
+    }
+  
+  
+  
+  
+    saudacao() {
+      console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+    }
+  }
+  
+
+
+
+
   return (
    <div className='conteiner'>
      
@@ -48,9 +68,9 @@ export default function Home() {
       
       </div>
 
-      <footer className="rodape">
-      <p>Integrantes: Emily,Evilyn,gabriel    Turma:2003 </p>
-    </footer>
+      <div className="rodape">
+        <p>Integrantes: Emily,Evilyn, gabriel    Turma:2003 </p>
+      </div>
 
    </div>
   );
